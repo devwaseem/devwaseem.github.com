2832595b17096cae10a5353a062e8b50dbfede23
Waseem <waseem07799@gmail.com>
2019-02-08 23:02:13 +0530

    added portfolio

---
+++ precache-manifest.37a5e2f673925d970160477c183fe4a8.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f77b1e20a74d671cca",
+    "url": "/static/css/main.6fe6221a.chunk.css"
+  },
+  {
+    "revision": "71f77b1e20a74d671cca",
+    "url": "/static/js/main.71f77b1e.chunk.js"
+  },
+  {
+    "revision": "e393d5b71e38821fe6e1",
+    "url": "/static/js/1.e393d5b7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebb4bb8ccfa2d86b74ef70680e02f964",
+    "url": "/static/media/bg2.ebb4bb8c.png"
+  },
+  {
+    "revision": "3a7496b70f3b215cfbd1788f199c207b",
+    "url": "/static/media/bg3.3a7496b7.png"
+  },
+  {
+    "revision": "998f4a99e4c344135239112ad65b4afd",
+    "url": "/static/media/profile.998f4a99.png"
+  },
+  {
+    "revision": "1b80fa400bf1f43d41f4555b71ba9c18",
+    "url": "/static/media/calc.1b80fa40.png"
+  },
+  {
+    "revision": "a17a2dfb88d31e54cfa6a958eed79ab9",
+    "url": "/static/media/checkout.a17a2dfb.png"
+  },
+  {
+    "revision": "f4ff9c5ef65821c91fed755367b481ce",
+    "url": "/static/media/login.f4ff9c5e.png"
+  },
+  {
+    "revision": "b7964bd57ec20c8926117e55465595d9",
+    "url": "/static/media/port.b7964bd5.png"
+  },
+  {
+    "revision": "85a438e39bbf42dffe18569552332c95",
+    "url": "/static/media/crescentaero.85a438e3.png"
+  },
+  {
+    "revision": "f4f510b6943dcce956e1f7217512c98b",
+    "url": "/static/media/face.f4f510b6.png"
+  },
+  {
+    "revision": "6e64667fca7ae22fede2dc76a281f16d",
+    "url": "/static/media/klipkutter.6e64667f.png"
+  },
+  {
+    "revision": "7ff550b74f9ed9285b9e0c040c7b1f59",
+    "url": "/static/media/notekeeper.7ff550b7.png"
+  },
+  {
+    "revision": "f88c443f02135a3ba091560e76ed767f",
+    "url": "/static/media/Poppins-Medium.f88c443f.otf"
+  },
+  {
+    "revision": "de2dd9339ae7636475fcd91b3ed0e24f",
+    "url": "/static/media/Poppins-Regular.de2dd933.otf"
+  },
+  {
+    "revision": "e47421f9b8cec2661620743c53475c8d",
+    "url": "/static/media/Poppins-Bold.e47421f9.otf"
+  },
+  {
+    "revision": "162a9aeb6c2ca8567f36a62f0d06da07",
+    "url": "/static/media/Poppins-ExtraBold.162a9aeb.otf"
+  },
+  {
+    "revision": "2e340246dfb20cda7ee331bcce1e1f06",
+    "url": "/index.html"
+  }
+];
